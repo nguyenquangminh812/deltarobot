62c8b9419458b99595400cc3b4ddb6cb0f654f53
nguyenquangminh812 <50289629+nguyenquangminh812@users.noreply.github.com>
2022-04-05 17:51:17 +0700

    Update detectObject.py

--- delta_robot/Camera/detectObject.py
+++ delta_robot/Camera/detectObject.py
@@ -6,6 +6,10 @@ cam = cv2.VideoCapture(0)
 while True:
     ret,image = cam.read()
     img = cv2.medianBlur(image, 3)
+    # parameter of image
+    width = img.shape[1]
+    height = img.shape[0]
+    origin = (int(width/2),int(height/2))
     # Convert to grayscale.
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower_red = cv2.inRange(hsv_img,np.array([0, 100, 100]), np.array([10, 255, 255]))
@@ -20,9 +24,15 @@ while True:
   
         for pt in circles[0, :]:
             a, b, r = pt[0], pt[1], pt[2]
+            
+            # draw point coordinate
+            cv2.circle(img, origin, 5, (255, 0, 0), -1)
   
             # Draw the circumference of the circle.
             cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+        
+            #position object in image 
+            cv2.putText(img,str(a-origin[0])+","+str(b-origin[1]),(a+10,b+10),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,255),2,cv2.LINE_AA)
   
             # Draw a small circle (of radius 1) to show the center.
             cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
